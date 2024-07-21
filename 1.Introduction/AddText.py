6bed814597ea212ece6223c16daa82908a738faf
platoonpool <163557956+platoonpool@users.noreply.github.com>
2024-07-21 23:37:33 +0530

    Initial commit

---
+++ 1.Introduction/AddText.py
@@ -0,0 +1,22 @@
+
+import pygame
+pygame.init()
+width, height = 1280, 720
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("My Awesome Game")
+fps = 30
+clock = pygame.time.Clock()
+start = True
+while start:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            start = False
+            pygame.quit()
+    window.fill((255, 255, 255))
+    font = pygame.font.Font('../Resources/Marcellus-Regular.ttf', 100)
+    text = font.render("My Awesome Game", True, (50, 50, 50))
+    window.blit(text, (350, 200))
+    pygame.display.update()
+    clock.tick(fps)
+
+
---
+++ 1.Introduction/Addimage.py
@@ -0,0 +1,20 @@
+#
+import pygame
+pygame.init()
+width, height = 1280, 720
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("My Awesome Game")
+fps = 30
+clock = pygame.time.Clock()
+imgBackground = pygame.image.load("../Resources/BackgroundBlue.jpg").convert()
+imgBalloonRed = pygame.image.load("../Resources/BalloonRed.png").convert_alpha()
+start = True
+while start:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            start = False
+            pygame.quit()
+    window.blit(imgBackground, (0, 0))
+    window.blit(imgBalloonRed, (200, 300))
+    pygame.display.update()
+    clock.tick(fps)
