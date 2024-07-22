9b50346131cca6894a5ee61e4e525a886acbf326
platoonpool <163557956+platoonpool@users.noreply.github.com>
2024-07-22 20:24:46 +0530

    Update Addreact.py

--- 1.Introduction/Addreact.py
+++ 1.Introduction/Addreact.py
@@ -1,43 +1,36 @@
 import pygame
 
-# Initialize
+
 pygame.init()
 
-# Create Window/Display
+
 width, height = 1280, 720
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("My Awesome Game")
 
-# Initialize Clock for FPS
+
 fps = 30
 clock = pygame.time.Clock()
 
-# Load Images
+
 imgBackground = pygame.image.load("../Resources/BackgroundBlue.jpg").convert()
 imgBalloonRed = pygame.image.load("../Resources/BalloonRed.png").convert_alpha()
 rectBalloon = imgBalloonRed.get_rect()
-
-# Rect
 rectNew = pygame.Rect(500, 0, 200, 200)
 
-# Main loop
+
 start = True
 while start:
-    # Get Events
+  
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             start = False
             pygame.quit()
-
-    # Apply Logic
     print(rectBalloon.colliderect(rectNew))
     rectBalloon.x += 2
-
-
     window.blit(imgBackground, (0, 0))
-    # pygame.draw.rect(window, (0, 255, 0), rectBalloon)
-    # pygame.draw.rect(window, (0, 255, 0), rectNew)
     window.blit(imgBalloonRed, rectBalloon)
+    
 
     # Update Display
     pygame.display.update()
