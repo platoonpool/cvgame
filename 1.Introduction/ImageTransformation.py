21dec2cd35316ff64e63921ac76acf2a7474170b
platoonpool <163557956+platoonpool@users.noreply.github.com>
2024-07-22 21:11:40 +0530

    Update ImageTransformation.py
    Signed-off-by: platoonpool <163557956+platoonpool@users.noreply.github.com>

--- 1.Introduction/ImageTransformation.py
+++ 1.Introduction/ImageTransformation.py
@@ -1,5 +1,4 @@
 import pygame
-
 pygame.init()
 width, height = 1280, 720
 window = pygame.display.set_mode((width, height))
@@ -9,18 +8,15 @@ clock = pygame.time.Clock()
 imgBackground = pygame.image.load("../Resources/BackgroundBlue.jpg").convert()
 imgBalloonRed = pygame.image.load("../Resources/BalloonRed.png").convert_alpha()
 imgBalloonRed = pygame.transform.rotozoom(imgBalloonRed, 90, 0.3)
-
-
 start = True
 while start:
-  
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             start = False
             pygame.quit()
+
     window.blit(imgBackground, (0, 0))
     window.blit(imgBalloonRed, (200, 300))
-
     pygame.display.update()
     clock.tick(fps)
-    
