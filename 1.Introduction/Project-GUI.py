d0660c1fd3a14b0e0fe863c8ca8232a38f583de7
platoonpool <163557956+platoonpool@users.noreply.github.com>
2024-07-22 20:31:19 +0530

    Update Project-GUI.py

--- 1.Introduction/Project-GUI.py
+++ 1.Introduction/Project-GUI.py
@@ -1,19 +1,11 @@
-# Import
 import pygame
-
-# Initialize
 pygame.init()
-
-# Create Window/Display
 width, height = 1280, 720
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("GUI")
-
-# Initialize Clock for FPS
 fps = 30
 clock = pygame.time.Clock()
 
-# Colors
 c = {"lightGreen": (189, 209, 197),
      "lightOrange": (238, 204, 140),
      "lightPink": (232, 178, 152),
@@ -25,8 +17,6 @@ c = {"lightGreen": (189, 209, 197),
      "white": (255, 255, 255),
      "black": (0, 0, 0),
      }
-
-# Images
 imgBackground = pygame.image.load('../Resources/Project - GUI/background.png').convert()
 imgDesign = pygame.image.load('../Resources/Project - GUI/design.png').convert_alpha()
 imgIcon1 = pygame.image.load('../Resources/Project - GUI/icon1.png').convert_alpha()
@@ -37,55 +27,39 @@ imgIcon5 = pygame.image.load('../Resources/Project - GUI/icon5.png').convert_alp
 imgShadow = pygame.image.load('../Resources/Project - GUI/shadow.png').convert_alpha()
 imgToggleOn = pygame.image.load('../Resources/Project - GUI/toggleOn.png').convert_alpha()
 imgToggleOff = pygame.image.load('../Resources/Project - GUI/toggleOff.png').convert_alpha()
-
-# List of WindowPads
 pads = [{"no": 1, "color": c['lightGreen'], "text": "Original", "icon": imgIcon2},
         {"no": 2, "color": c['lightOrange'], "text": "Gray Scale", "icon": imgIcon3},
         {"no": 3, "color": c['lightPink'], "text": "Edges", "icon": imgIcon4},
         {"no": 4, "color": c['darkPink'], "text": "Contours", "icon": imgIcon5}]
 
 
-def drawWindowPad(pos, color, text, icon):
+def drawindowpad(pos, color, text, icon):
     xo, yo, w, h = pos
-    # Add Shadow
     window.blit(imgShadow, (xo, yo + h - 66))
-    # Header Rectangle
     pygame.draw.rect(window, color, (xo, yo, w, 64),
                      border_top_left_radius=10, border_top_right_radius=10)
-    # Image Area White
     pygame.draw.rect(window, c['white'], (xo, yo + 64, w, h - 87),
                      border_bottom_left_radius=10, border_bottom_right_radius=10)
-    # Icon
     window.blit(icon, (xo + 20, yo + 12))
-    # Text
     font = pygame.font.Font('../Resources/Marcellus-Regular.ttf', 20)
     text = font.render(text, True, c['darkBrown'])
     window.blit(text, (xo + 82, yo + 20))
 
-
-def drawFilterPad():
-    drawWindowPad((75, 57, 312, 602), c["darkGreen"], "Filter", imgIcon1)
-
-    # Toggles
+     def drawfilterpad():
+    drawwindowpad((75, 57, 312, 602), c["darkGreen"], "Filter", imgIcon1)
     font = pygame.font.Font('../Resources/Marcellus-Regular.ttf', 20)
-    # 1
-    textDisp1 = font.render("Gray Scale", True, c["darkBrown"])
-    window.blit(textDisp1, (106, 165))
+    textdisp1 = font.render("Gray Scale", True, c["darkBrown"])
+    window.blit(textdisp1, (106, 165))
     window.blit(imgToggleOn, (283, 164))
-    # 2
-    textDisp2 = font.render("Edges", True, c["darkBrown"])
-    window.blit(textDisp2, (106, 165 + 60))
+    textdisp2 = font.render("Edges", True, c["darkBrown"])
+    window.blit(textdisp2, (106, 165 + 60))
     window.blit(imgToggleOn, (283, 164 + 60))
-    # 3
-    textDisp3 = font.render("Contours", True, c["darkBrown"])
-    window.blit(textDisp3, (106, 165 + 60 * 2))
+    textdisp3 = font.render("Contours", True, c["darkBrown"])
+    window.blit(textdisp3, (106, 165 + 60 * 2))
     window.blit(imgToggleOff, (283, 164 + 60 * 2))
-    # 4
-    textDisp4 = font.render("Blur", True, c["darkBrown"])
-    window.blit(textDisp4, (106, 165 + 60 * 3))
+    textdisp4 = font.render("Blur", True, c["darkBrown"])
+    window.blit(textdisp4, (106, 165 + 60 * 3))
     window.blit(imgToggleOff, (283, 164 + 60 * 3))
-
-    # Sliders
     font = pygame.font.Font('../Resources/Marcellus-Regular.ttf', 25)
     for y in range(0, 3):
         h = 447 + y * 55
@@ -95,40 +69,26 @@ def drawFilterPad():
         pygame.draw.rect(window, c["darkGray"], (sliderPos, h - 15, 12, 30))
         textDisp = font.render(str(y * 50 + 30), True, c["darkBrown"])
         window.blit(textDisp, (286, h - 18))
-
-
-def drawAll():
+def drawall():
     w, h = 312, 301
-    gapW, gapH = 72, 25
-    drawWindowPad((484, 57, w, h), pads[0]['color'], pads[0]['text'], pads[0]['icon'])
-    drawWindowPad((484 + gapW + w, 57, w, h), pads[1]['color'], pads[1]['text'], pads[1]['icon'])
-    drawWindowPad((484, 57 + gapH + h, w, h), pads[2]['color'], pads[2]['text'], pads[2]['icon'])
-    drawWindowPad((484 + gapW + w, 57 + gapH + h, w, h), pads[3]['color'], pads[3]['text'], pads[3]['icon'])
+    gapw, gaph = 72, 25
+    drawindowpad((484, 57, w, h), pads[0]['color'], pads[0]['text'], pads[0]['icon'])
+    drawindowpad((484 + gapw + w, 57, w, h), pads[1]['color'], pads[1]['text'], pads[1]['icon'])
+    drawindowpad((484, 57 + gaph + h, w, h), pads[2]['color'], pads[2]['text'], pads[2]['icon'])
+    drawindowpad((484 + gapw + w, 57 + gaph + h, w, h), pads[3]['color'], pads[3]['text'], pads[3]['icon'])
+    drawfilterpad()
 
-    drawFilterPad()
 
-
-# Main loop
 start = True
 while start:
-    # Get Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             start = False
             pygame.quit()
-
-    # Apply Logic
     window.blit(imgBackground, (0, 0))
     imgDesign.set_alpha(0)
     window.blit(imgDesign, (0, 0))
-
-    drawAll()
-
-    # Update Display
+    drawall()
     pygame.display.update()
-    # Set FPS
     clock.tick(fps)
-
-
-
-    
+   
